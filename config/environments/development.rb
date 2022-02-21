3e2d0e35b3e739d0bd299b67da4bd1ef0cfe7e5e
sharma-hardik <hardik1235.hs@gmail.com>
2022-02-21 22:20:37 +0530

    toy app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,3 +74,7 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
+Rails.application.configure do
+  # Allow Cloud9 connections.
+  config.hosts.clear
+end
